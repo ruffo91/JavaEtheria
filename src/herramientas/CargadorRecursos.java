95790464192460458cc3f2ae6f7261dbcf8839d0
Ruben Lopez <elperrothehound@hotmail.com>
2014-12-18 01:11:36 +0100

    Añadido performance a la herramienta de generación de imágenes a partir de la configuración de la gráfica

--- src/herramientas/CargadorRecursos.java
+++ src/herramientas/CargadorRecursos.java
@@ -4,13 +4,14 @@ import java.awt.Graphics;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class CargadorRecursos {
-	public static BufferedImage cargarImagenCompatible(final String ruta)
+	public static BufferedImage cargarImagenCompatibleOpaca(final String ruta)
 	{
 		Image imagen = null;
 		
@@ -25,7 +26,31 @@ public class CargadorRecursos {
 		
 		GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
 		
-		BufferedImage imagenAcelerada = gc.createCompatibleImage(imagen.getWidth(null), imagen.getHeight(null));
+		BufferedImage imagenAcelerada = gc.createCompatibleImage(imagen.getWidth(null), imagen.getHeight(null), Transparency.OPAQUE);
+		
+		Graphics g = imagenAcelerada.getGraphics();
+		g.drawImage(imagen, 0, 0, null);
+		g.dispose();
+		
+		return imagenAcelerada;
+	}
+	
+	public static BufferedImage cargarImagenCompatibleTranslucida(final String ruta)
+	{
+Image imagen = null;
+		
+		try
+		{
+			imagen = ImageIO.read(ClassLoader.class.getResource(ruta));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+		
+		BufferedImage imagenAcelerada = gc.createCompatibleImage(imagen.getWidth(null), imagen.getHeight(null), Transparency.TRANSLUCENT);
 		
 		Graphics g = imagenAcelerada.getGraphics();
 		g.drawImage(imagen, 0, 0, null);
